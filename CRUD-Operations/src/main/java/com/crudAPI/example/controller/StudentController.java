5b0a7618a392d3111f166739528ba7a47e83b9a6
Aman gupta <90634749+amangupta7024@users.noreply.github.com>
2024-07-27 20:31:31 +0530

    Uplaod the project

---
+++ CRUD-Operations/src/main/java/com/crudAPI/example/controller/StudentController.java
@@ -0,0 +1,47 @@
+package com.crudAPI.example.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.crudAPI.example.entity.Students;
+import com.crudAPI.example.service.StudentService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+public class StudentController {
+	
+	@Autowired
+	private StudentService studentService;
+	
+	@PostMapping("/addStudent")
+	public Students addStudent(@RequestBody Students students) {
+		return studentService.addStudent(students);
+	}
+	
+	@GetMapping("/getStudents")
+	public List<Students> getAllStudents(){
+		return studentService.getAllStudents();
+	}
+	
+	@PostMapping("/updateStudent")
+	public Students updateStudent(@RequestBody Students students) {
+		return studentService.udpateStudent(students);
+	}
+	
+	@GetMapping("/deleteStudent/{id}")
+	public Boolean deleteStudent(@PathVariable int id) {
+		return studentService.deleteStudent(id);
+	}
+	
+	
+
+}
